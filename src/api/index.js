f38c7086af7651b82612a41cd4c4ddfbf8d676e5
Briam <briammartinez@gmail.com>
2019-12-02 01:53:10 +0100

    feat: Created inital version of api file

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+
+export const fetchLocations = async (query) => {
+  const res = await fetch(`https://www.metaweather.com/api/location/search/?query=${query}`);
+  return res.json()
+  .then(fetchDataByWoeid)
+}
+
+const getDetail = async (id) => {
+  const res = await fetch(`https://www.metaweather.com/api/location/${id}/`);
+  const item = await res.json();
+  return item;
+}
+
+const fetchDataByWoeid = async (arr) => {
+  const promises = arr.map(({ woeid }) => getDetail(woeid));
+  const items = await Promise.all(promises);
+  return items;
+}
