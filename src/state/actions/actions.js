228b93f9f433f1b6c5de6e067e05ce842996ee23
Briam <briammartinez@gmail.com>
2019-12-02 01:52:09 +0100

    feat: Created get locations action

--- src/state/actions/actions.js
+++ src/state/actions/actions.js
@@ -1,3 +1,12 @@
-export const ADD_LIST = 'ADD_LIST';
+import { fetchLocations } from '../../api';
 
+export const ADD_LIST = 'ADD_LIST';
 export const addList = (payload) => ({ type: ADD_LIST, payload });
+
+export const GET_LOCATIONS = 'GET_LOCATIONS';
+export const getLocations = (query) => async (dispatch) => {
+  dispatch({ type: 'SET_LOADING', payload: true })
+  const items = await fetchLocations(query);
+  dispatch({ type: 'SET_LOADING', payload: false })
+  dispatch(addList(items))
+};
--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import list from './reducers/list';
 
-let store = createStore(list, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(list, composeEnhancers(applyMiddleware(thunk)));
 export default store;
